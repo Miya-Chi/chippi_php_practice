280d3ecbf368b5786eded7c344f187d38884ef67
みやしたちひろ <c09069354869@gmail.com>
2019-05-28 15:47:45 +0800

    全然できませんでした！

---
+++ php_practice.php
@@ -0,0 +1,21 @@
+<?php
+    $i = 100;
+
+    for ($i < 101; $i > 0; $i--) {
+       echo $i . '<br>';
+    }
+
+    $i = 1;
+    for ($i = 0;$i < 10; $i*3){
+        echo $i . '<br>';
+    }
+
+
+    $english = 70; //英語の点数
+    $math = 70; //数学の点数
+    if( $english >= 60; $math>= 60 && $ >= 140 ){
+        echo '合格！';
+    } else {
+        echo "残念！";
+    }
+    echo '<br>';
